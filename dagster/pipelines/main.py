91ae44e83e8893e160932f4547e3036fec2c1cb7
Nicolas Ramy <nicolas.ramy@darkelda.com>
2020-10-19 16:43:58 +0200

    Add default files to configure dagster

---
+++ dagster/pipelines/main.py
@@ -0,0 +1,20 @@
+from dagster import execute_pipeline, pipeline, solid
+
+
+@solid
+def get_name(_):
+    return "dagster"
+
+
+@solid
+def hello(context, name: str):
+    context.log.info("Hello, {name}!".format(name=name))
+
+
+@pipeline
+def hello_pipeline():
+    hello(get_name())
+
+
+if __name__ == "__main__":
+    result = execute_pipeline(hello_pipeline)
